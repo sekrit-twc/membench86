8be849b42fb8de887abe308ea8c3bf7bac4987db
sekrit-twc <noreply@example.com>
2018-01-24 21:40:05 -0800

    Fix unused registers in AVX-512 kernel.

--- membench86/kernel_avx512.cpp
+++ membench86/kernel_avx512.cpp
@@ -24,28 +24,26 @@ unsigned read_memory_avx512(const void *buf, size_t count)
 	__m512i x14 = x0;
 	__m512i x15 = x0;
 
-#define AT(x) (const __m512i *)((const char *)buf_p + (x))
-	for (size_t i = 0; i < count; i += 64 * 16) {
-		x0 = _mm512_xor_si512(x0, _mm512_load_si512(AT(64 * 0)));
-		x1 = _mm512_xor_si512(x1, _mm512_load_si512(AT(64 * 1)));
-		x2 = _mm512_xor_si512(x2, _mm512_load_si512(AT(64 * 2)));
-		x3 = _mm512_xor_si512(x3, _mm512_load_si512(AT(64 * 3)));
-		x4 = _mm512_xor_si512(x4, _mm512_load_si512(AT(64 * 4)));
-		x5 = _mm512_xor_si512(x5, _mm512_load_si512(AT(64 * 5)));
-		x6 = _mm512_xor_si512(x6, _mm512_load_si512(AT(64 * 6)));
-		x7 = _mm512_xor_si512(x7, _mm512_load_si512(AT(64 * 7)));
-		x0 = _mm512_xor_si512(x0, _mm512_load_si512(AT(64 * 8)));
-		x1 = _mm512_xor_si512(x1, _mm512_load_si512(AT(64 * 9)));
-		x2 = _mm512_xor_si512(x2, _mm512_load_si512(AT(64 * 10)));
-		x3 = _mm512_xor_si512(x3, _mm512_load_si512(AT(64 * 11)));
-		x4 = _mm512_xor_si512(x4, _mm512_load_si512(AT(64 * 12)));
-		x5 = _mm512_xor_si512(x5, _mm512_load_si512(AT(64 * 13)));
-		x6 = _mm512_xor_si512(x6, _mm512_load_si512(AT(64 * 14)));
-		x7 = _mm512_xor_si512(x7, _mm512_load_si512(AT(64 * 15)));
+	for (size_t i = 0; i < count; i += 16 * 64) {
+		x0 = _mm512_xor_si512(x0, _mm512_load_si512(buf_p + 0 * 64));
+		x1 = _mm512_xor_si512(x1, _mm512_load_si512(buf_p + 1 * 64));
+		x2 = _mm512_xor_si512(x2, _mm512_load_si512(buf_p + 2 * 64));
+		x3 = _mm512_xor_si512(x3, _mm512_load_si512(buf_p + 3 * 64));
+		x4 = _mm512_xor_si512(x4, _mm512_load_si512(buf_p + 4 * 64));
+		x5 = _mm512_xor_si512(x5, _mm512_load_si512(buf_p + 5 * 64));
+		x6 = _mm512_xor_si512(x6, _mm512_load_si512(buf_p + 6 * 64));
+		x7 = _mm512_xor_si512(x7, _mm512_load_si512(buf_p + 7 * 64));
+		x8 = _mm512_xor_si512(x8, _mm512_load_si512(buf_p + 8 * 64));
+		x9 = _mm512_xor_si512(x9, _mm512_load_si512(buf_p + 9 * 64));
+		x10 = _mm512_xor_si512(x10, _mm512_load_si512(buf_p + 10 * 64));
+		x11 = _mm512_xor_si512(x11, _mm512_load_si512(buf_p + 11 * 64));
+		x12 = _mm512_xor_si512(x12, _mm512_load_si512(buf_p + 12 * 64));
+		x13 = _mm512_xor_si512(x13, _mm512_load_si512(buf_p + 13 * 64));
+		x14 = _mm512_xor_si512(x14, _mm512_load_si512(buf_p + 14 * 64));
+		x15 = _mm512_xor_si512(x15, _mm512_load_si512(buf_p + 15 * 64));
 
-		buf_p += 64 * 16;
+		buf_p += 16 * 64;
 	}
-#undef AT
 
 	x0 = _mm512_add_epi8(x0, x1);
 	x2 = _mm512_add_epi8(x2, x3);
